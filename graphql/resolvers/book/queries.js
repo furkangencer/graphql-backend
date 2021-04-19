69f95acccd3b398d9314a6a723bfc76cd984f7fb
Furkan Gençer <furkan.gencer@getir.com>
2021-04-19 19:01:30 +0300

    add pagination

--- graphql/resolvers/book/queries.js
+++ graphql/resolvers/book/queries.js
@@ -1,9 +1,32 @@
 import { Book } from '../../../db/models';
 
 const bookQueries = {
-  books: async (_, args, { loaders }) => {
-    const books = await Book.find();
-    return loaders.book.many(books.map(({ id }) => id));
+  books: async (_, { params = { page: 1, pageSize: 20 } }, { loaders }) => {
+    const { pageSize, page } = params;
+
+    return {
+      results: async () => {
+        const books = await Book.find()
+          .skip(pageSize * (page - 1))
+          .limit(pageSize);
+
+        return loaders.book.many(books.map(({ id }) => id));
+      },
+      info: async () => {
+        const count = await Book.countDocuments();
+
+        const pages = Math.ceil(count / pageSize);
+        const prev = page > 1 ? page - 1 : null;
+        const next = page < pages ? page + 1 : null;
+
+        return {
+          count,
+          pages,
+          prev,
+          next,
+        };
+      },
+    };
   },
   book: async (_, { id }, { loaders }) => loaders.book.one(id),
 };
